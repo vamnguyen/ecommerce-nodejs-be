115fb4d03790636d3738ae3f9bcf620ef3a6679c
Minh Nguyen <minhnva.operor@gmail.com>
2024-10-15 19:13:24 +0700

    feat: message queue kafka

---
+++ src/test/message-queue/kafka/producer.js
@@ -0,0 +1,21 @@
+const { Kafka, logLevel } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+  logLevel: logLevel.NOTHING,
+});
+
+const producer = kafka.producer();
+
+const runProducer = async () => {
+  await producer.connect();
+  await producer.send({
+    topic: "test-topic",
+    messages: [{ value: "Hello KafkaJS user by Minh!" }],
+  });
+
+  await producer.disconnect();
+};
+
+runProducer().catch(console.error);
