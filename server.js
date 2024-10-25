eac53cdd3098b046c96bab909f2bf7ecfe914f10
Minh Nguyen <minhnva.operor@gmail.com>
2024-10-25 11:42:20 +0700

    fix: change port in server.js file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const {
-  app: { port },
-} = require("./src/configs/config.mongodb");
+// const {
+//   app: { port },
+// } = require("./src/configs/config.mongodb");
+const port = 3000;
 
 const server = app.listen(port, () => {
   console.log(`Web service running on port: ${[port]}.`);
